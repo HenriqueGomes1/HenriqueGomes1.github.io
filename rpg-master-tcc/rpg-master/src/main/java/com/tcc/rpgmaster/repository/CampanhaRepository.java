f1edf0f1194dae1c293fb860cb931f832e00ef45
Henrique Gomes <henrique.vale@univille.br>
2026-07-08 23:39:52 -0300

    Delete rpg-master-tcc/rpg-master directory

--- rpg-master-tcc/rpg-master/src/main/java/com/tcc/rpgmaster/repository/CampanhaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tcc.rpgmaster.repository;
-
-import com.tcc.rpgmaster.model.Campanha;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CampanhaRepository extends JpaRepository<Campanha, Long> {
-}
